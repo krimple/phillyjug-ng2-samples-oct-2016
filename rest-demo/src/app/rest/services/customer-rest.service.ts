21d83a3c0ee4bf4652adfa07a3531cb847723b77
Ken Rimple <krimple@chariotsolutions.com>
2016-10-19 09:00:15 -0400

    fixup proxy configuration

--- rest-demo/src/app/rest/services/customer-rest.service.ts
+++ rest-demo/src/app/rest/services/customer-rest.service.ts
@@ -1,9 +1,8 @@
 import {Injectable} from '@angular/core';
-import { Http, Response, RequestOptionsArgs } from '@angular/http';
+import { Http, Response } from '@angular/http';
 import {Observable} from 'rxjs/Rx';
 import 'rxjs/add/operator/map';
 import {Customer} from '../models/customer';
-import HttpResponse = http.HttpResponse;
 
 @Injectable()
 export class CustomerRESTService {
@@ -11,7 +10,7 @@ export class CustomerRESTService {
 
   // todo - convert to promise - not recognizing Promise type in ES2015 from typeScript
   load(): Observable<Customer[]> {
-    let response = this.http.get('/server/customers')
+    let response = this.http.get('/customers')
       .map((datum: Response) => {
         let body = datum.json();
         return body.map((item) => {
@@ -25,9 +24,9 @@ export class CustomerRESTService {
   // todo - convert to promise - not recognizing Promise type in ES2015 from TypeScript
   saveOrUpdate(customer) {
     if (customer.id) {
-      return this.http.put('/server/customers/' + customer.id, JSON.stringify(customer));
+      return this.http.put('/customers/' + customer.id, customer);
     } else {
-      return this.http.post('/server/customers/', JSON.stringify(customer));
+      return this.http.post('/customers/', customer);
     }
   }
 }
